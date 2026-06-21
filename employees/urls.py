aff230ec7a0f928c0738d36e5ec72b0e9800a201
nitin <nitin13091981@gmail.com>
2026-06-21 06:08:56 +0530

    urls.py

--- employees/urls.py
+++ employees/urls.py
@@ -1,13 +1,22 @@
-from django import views
 from django.urls import path
-from .views import home, attendance_entry, add_labour, login_view,attendance_report,edit_attendance,delete_attendance
-urlpatterns = [
-    path('', login_view, name='login'),
-    path('home/', home, name='home'),
-    path('attendance/', attendance_entry, name='attendance'),
-    path('labour/add/', add_labour, name='add_labour'),
-    path('attendance-report/', attendance_report, name='attendance_report'),
-    path("attendance/edit/[int:id](int:id)/",views.edit_attendance,name="edit_attendance"),
-    path("attendance/delete/[int:id](int:id)/",views.delete_attendance,name="delete_attendance"),
+from . import views
 
+urlpatterns = [
+    path('', views.login_view, name='login'),
+    path('home/', views.home, name='home'),
+    path('attendance/', views.attendance_entry, name='attendance'),
+    path('labour/add/', views.add_labour, name='add_labour'),
+    path('attendance-report/', views.attendance_report, name='attendance_report'),
+
+    path(
+        'attendance/edit/<int:id>/',
+        views.edit_attendance,
+        name='edit_attendance'
+    ),
+
+    path(
+        'attendance/delete/<int:id>/',
+        views.delete_attendance,
+        name='delete_attendance'
+    ),
 ]
